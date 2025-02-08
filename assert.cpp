3bd87876ab325f6b5e33b0484f93ec62b832755d
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-02-07 22:06:33 -0800

    static_assert

--- assert.cpp
+++ assert.cpp
@@ -14,11 +14,16 @@ int main(){
     //Print to console to confirm success.
     std::cout << "First assert passed." << std::endl;
     
-    //Test assert, result is false.
-    assert(y<myDouble);
+    //Test assert, result is false. Remove the line below to test the rest of the code.
+    // assert(y<myDouble);
 
     //Fail to print to console to confirm second assert failure.
-    std::cout << "First assert passed." << std::endl;
+    std::cout << "Second assert passed. (This should not print)" << std::endl;
+
+    int myArray[] ={0,1,2,3};
+
+    //Test static_assert. Evaluated at compile time. Size of array is 16bytes. Compilation will fail.
+    static_assert(sizeof(myArray)<4,"Static assert failure. Size of array is greater than 4.");
 
     return 0;
 }

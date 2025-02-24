0b05a3339b50d89bd481c565ca84e134f2078cfb
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-02-23 21:48:05 -0800

    added constexpr whereapplicable

--- assert.cpp
+++ assert.cpp
@@ -3,24 +3,23 @@
 
 
 int main(){
-    int x={5};
-    int y={10};
-    int z={3};
-    double myDouble={2.1};
+    constexpr int x={5};
+    constexpr int y={10};
+    constexpr double myDouble={2.1};
 
     //Test assert, result is true.
     assert(y>x);
     
     //Print to console to confirm success.
-    std::cout << "First assert passed." << std::endl;
+    std::cout << "First assert passed.\n";
     
     //Test assert, result is false. Remove the line below to test the rest of the code.
     // assert(y<myDouble);
 
     //Fail to print to console to confirm second assert failure.
-    std::cout << "Second assert passed. (This should not print)" << std::endl;
+    std::cout << "Second assert passed. (This should not print if previous statement is uncommented)\n";
 
-    int myArray[] ={0,1,2,3};
+    constexpr int myArray[4] ={0,1,2,3};
 
     //Test static_assert. Evaluated at compile time. Size of array is 16bytes. Compilation will fail.
     static_assert(sizeof(myArray)<4,"Static assert failure. Size of array is greater than 4.");

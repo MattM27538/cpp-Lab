1eb13c5cf49600c9c63dfe0602d7e2c98b492d9b
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-02-23 21:53:41 -0800

    uncommented second assertion

--- assert.cpp
+++ assert.cpp
@@ -15,7 +15,7 @@ int main(){
     std::cout << "First assert passed.\n";
     
     //Test assert, result is false. Remove the line below to test the rest of the code.
-    // assert(y<myDouble);
+    assert(y<myDouble);
 
     //Fail to print to console to confirm second assert failure.
     std::cout << "Second assert passed. (This should not print if previous statement is uncommented)\n";

6f4c7accac52f897bd55fed90f776e101193e2eb
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-02-27 14:58:56 -0800

    updated variables names for consistency

--- constexpr.cpp
+++ constexpr.cpp
@@ -4,27 +4,27 @@ int main(){
 
     //Constexpr are determined at compile time. 
     //Useful for predictability, quality/performance(both check for errors and evaluate at compile time.)
-    constexpr int One={1};
+    constexpr int myInt1={1};
 
     std::cout << "hello world"; // This can be determined at runtime or compile time depending on compiler.
 
-    const int a={5}; // Can be used in a const expression.
+    const int myInt2={5}; // Can be used in a const expression.
 
     //Test
-    constexpr int test=a;
+    constexpr int myInt3={myInt2};
 
-    std::cout << "\n" << a << "\n";
+    std::cout << "\n" << myInt3 << "\n";
 
-    int b={5}; // Cannot be used in const expression because b is not const(might not be determined at compile time).
+    int myInt4={5}; // Cannot be used in const expression because b is not const(might not be determined at compile time).
 
     // Uncomment to test for compile time error.
-    // constexpr int test2=b;
+    // constexpr int myTestInt=myInt4;
     
     // Also cannot be used in const expression
-    const int e {b};
+    const int myInt5=myInt4;
     
     // Uncomment to test for compile time error.
-    // constexpr int test3=e;
+    // constexpr int myTestInt2=myInt5;
 
 
     // This is not necessarily determined at compile time. Determination is only required in contexts that require a constant expression.

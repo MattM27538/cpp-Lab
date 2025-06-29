e632aa14b98d5065fb5bd1a4cfa3d51f0733119f
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-06-29 10:58:05 -0700

    Reassign dynamic variable

--- newOperator.cpp
+++ newOperator.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iostream>
 
 int main(){
     /*To allocate a single variable dynamically, we use the scalar (non-array) form of the new 
@@ -7,15 +8,21 @@ int main(){
     */
     int* myPtr{new int{5}};
 
+    std::cout<<"Address of myPtr after initialization = "<<&myPtr <<"\n";
+    
+    //Reassign dynamic variable.
+    *myPtr=7;
+    std::cout<<"Address of myPtr after reassignment = "<<&myPtr <<"\n";
+
     /*When we are done with a dynamically allocated variable, we need to explicitly tell C++ to
-      free the memory for reuse. For single variables, this is done via the scalar (non-array) 
-      form of the delete operator:
+    free the memory for reuse. For single variables, this is done via the scalar (non-array) 
+    form of the delete operator:
     */
     delete myPtr; // return the memory pointed to by myPtr to the operating system.
     myPtr=nullptr; // set ptr to be a null pointer as accessing it now would be undefined behavior (dangling pointer).
-
+   
     /*Best practice: Deleting a null pointer is okay, and does nothing. There is no need to 
-      conditionalize your delete statements.
+    conditionalize your delete statements.
     */
     delete myPtr;
 
